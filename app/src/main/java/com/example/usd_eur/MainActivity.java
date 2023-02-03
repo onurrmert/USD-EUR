6035301e7a74dc48dd5ba8203f91a6e92d014772
onur mert boybek <onurboybek@gmail.com>
2023-02-03 18:09:02 +0300

    all libraries added

---
+++ app/src/main/java/com/example/usd_eur/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.usd_eur;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
+
+import android.os.Bundle;
+import com.example.usd_eur.databinding.ActivityMainBinding;
+
+import retrofit2.Retrofit;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        final ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
